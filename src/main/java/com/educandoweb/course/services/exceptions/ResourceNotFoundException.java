6cfa44d235eba6e25ad9db83a32b7f3a51a23d2f
Raphael Santos <raphaelsantos0712@gmail.com>
2021-11-09 11:08:55 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	
+	public static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not fount, Id " + id);
+	}
+	
+}
